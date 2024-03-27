afbb31ef35cd0bcdfa12d62681fbe2c2a4a925c9
Aritra Mondal <aritramondal855@gmail.com>
2024-03-27 21:43:43 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void exec(int argc, const char* argv[])
+{
+    FILE* fout = fopen(argv[1], "w");
+
+    if (fout == NULL) {
+        printf("Could not create file!"
+               " Do you have write permission in this directory");
+    }
+
+    if (argc > 3) {
+        for (int i = 3; i < argc; i++) {
+            fprintf(fout, "#include <%s>\n", argv[i]);
+        }
+    }
+
+    fprintf(fout, "\nint main(void)\n"
+                  "{\n    printf(\"Hello, world!\\n\");\n"
+                  "\n    return 0;\n}\n");
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2) {
+        printf("Usage: %s <file.c> -i <header1.h> <header2.h> ...\n"
+               "Without the angle brackets!\n", argv[0]);
+        exit(1);
+
+    } else if (argc == 3) {
+        printf("Usage: cb <file.c> -i <header1.h> <header2.h> ...\n"
+               "Without the angle brackets!\n");
+        exit(1);
+    } else {
+        exec(argc, argv);
+    }
+    return 0;
+}
